6e6791f0a4760a0042e29bcb901785539adc0c6a
Gabriele Jandres <gabriele.jandres@nubank.com.br>
2023-05-04 18:30:50 -0300

    + aula 3

---
+++ aulas/aula3/src/aula3/confidential.clj
@@ -0,0 +1,9 @@
+(ns aula3.confidential)
+
+(def ^:private password-by-user
+  {:Maria "1234" :Pedro "asjcjaku" :Joana "amomeupet"})
+
+(defn- get-password
+  "Get user's password by username"
+  [user]
+  (get password-by-user (keyword user)))
